7eeaae8465b659645eaee5d7dc4c8a8cc6f5c75c
helitonqueiroz <heliton.queiroz10@gmail.com>
2025-01-29 13:20:44 -0300

    NavBar

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.navbar__toggle');
+    const menu = document.querySelector('.navbar__menu');
+
+    toggleButton.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
